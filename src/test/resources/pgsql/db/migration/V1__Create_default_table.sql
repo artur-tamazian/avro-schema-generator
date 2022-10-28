c1cb7305f408f686cb767cff7bec4ce83d341827
rajkaran <rajkaran.sharma@razorpay.com>
2022-10-28 15:09:16 +0530

    add test for postgres default string

--- src/test/resources/pgsql/db/migration/V1__Create_default_table.sql
+++ src/test/resources/pgsql/db/migration/V1__Create_default_table.sql
@@ -4,6 +4,7 @@ create table default_table
     "first_name"          varchar(50) not null,
     "last_name"           varchar(50),
     "address"             varchar(50) default null,
+    "default_string"      varchar(50) default 'hello',
     "created"             timestamp   not null,
     "updated"             timestamp,
     "decimal_field"       decimal(20, 3),
